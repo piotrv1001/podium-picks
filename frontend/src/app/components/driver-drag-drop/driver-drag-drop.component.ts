906d42f9939ffa4f5ed2f02aaab65b4a68832488
piotrv1001 <piotrv1001@interia.pl>
2023-04-16 12:56:10 +0200

    Better handling of driver order change event

--- frontend/src/app/components/driver-drag-drop/driver-drag-drop.component.ts
+++ frontend/src/app/components/driver-drag-drop/driver-drag-drop.component.ts
@@ -1,20 +1,27 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, Output, OnInit } from "@angular/core";
 import { Driver } from "src/app/model/entities/driver.model";
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { DragDropEvent } from "src/app/model/types/drag-drop-event";
+import { RaceEventService } from "src/app/services/race-event.service";
 
 @Component({
   selector: 'app-driver-drag-drop',
   templateUrl: './driver-drag-drop.component.html',
   styleUrls: ['./driver-drag-drop.component.scss']
 })
-export class DriverDragDropComponent {
+export class DriverDragDropComponent implements OnInit {
 
   @Input() drivers: Driver[] = [];
   @Input() isEditable: boolean = false;
   @Output() dropped: EventEmitter<DragDropEvent> = new EventEmitter();
   @Output() saved: EventEmitter<Driver[]> = new EventEmitter();
-  @Input() madeChanges: boolean = false;
+  madeChanges: boolean = false;
+
+  constructor(private raceEventService: RaceEventService) { }
+
+  ngOnInit(): void {
+    this.subscribeToMadeChanges();
+  }
 
   drop(event: CdkDragDrop<Driver[]>) {
     const prevIndex = event.previousIndex;
@@ -28,6 +35,10 @@ export class DriverDragDropComponent {
 
   saveBtnClicked(): void {
     this.saved.emit(this.drivers);
+  }
+
+  private subscribeToMadeChanges(): void {
+    this.raceEventService.getMadeChangesObservable().subscribe(madeChanges => this.madeChanges = madeChanges);
   }
 
 }
--- frontend/src/app/services/race-event.service.ts
+++ frontend/src/app/services/race-event.service.ts
@@ -5,16 +5,25 @@ import { CustomDate } from '../model/types/custom-date';
 @Injectable({
   providedIn: 'root'
 })
-export class RaceTimeService {
+export class RaceEventService {
 
   private raceTimeSub: Subject<CustomDate> = new Subject<CustomDate>();
+  private madeChangesSub: Subject<boolean> = new Subject<boolean>();
 
   getRaceTimeObservable(): Observable<CustomDate> {
     return this.raceTimeSub.asObservable();
   }
 
+  getMadeChangesObservable(): Observable<boolean> {
+    return this.madeChangesSub.asObservable();
+  }
+
   notifyAboutRaceTime(raceTime: CustomDate): void {
     this.raceTimeSub.next(raceTime);
+  }
+
+  notifyAboutMadeChanges(madeChanges: boolean): void {
+    this.madeChangesSub.next(madeChanges);
   }
 
 }
