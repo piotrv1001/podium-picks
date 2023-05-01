8551575c07ec80313bc093d8ebc7803ef6a4030d
PiotrV <piotrv1001@gmail.com>
2023-05-01 18:01:29 +0200

    Next race border animation

--- frontend/src/app/pipes/to-fixed-string.pipe.ts
+++ frontend/src/app/pipes/to-fixed-string.pipe.ts
@@ -4,10 +4,10 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'toFixedString'
 })
 export class ToFixedStringPipe implements PipeTransform {
-  transform(value: number | string): string {
+  transform(value: number | string, fractionDigits: number = 1): string {
     if(typeof value === 'number') {
-      return value.toFixed(1);
+      return value.toFixed(fractionDigits);
     }
-    return Number(value).toFixed(1);
+    return Number(value).toFixed(fractionDigits);
   }
 }
