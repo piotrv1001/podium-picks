47c3f438f580064f9b7698c0d85e0301d440b733
PiotrV <piotrv1001@gmail.com>
2023-05-07 12:57:34 +0200

    Admin FL and DNF view

--- backend/src/race/race.module.ts
+++ backend/src/race/race.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Race } from './race.entity';
 import { RaceService } from './race.service';
 import { RaceController } from './race.controller';
+import { Driver } from 'src/driver/driver.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Race])],
+  imports: [
+    TypeOrmModule.forFeature([Race]),
+    TypeOrmModule.forFeature([Driver]),
+  ],
   providers: [RaceService],
   controllers: [RaceController],
 })
